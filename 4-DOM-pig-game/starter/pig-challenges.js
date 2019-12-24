5468a36efbe59645f3f3f6f7dc440785f2632257
abarela13 <abarela13@gmail.com>
2019-12-23 20:01:34 -0800

    Dice Challenge Part 2 Completed

--- 4-DOM-pig-game/starter/pig-challenges.js
+++ 4-DOM-pig-game/starter/pig-challenges.js
@@ -12,7 +12,7 @@ GAME RULES:
 var scores, roundScore, activePlayer;
 
 // Dice Values
-var diceValue, diceRolls;
+var diceValue1, diceRolls, lastRoll;
 
 // DOM Button Elements Getters
 var btnStart = document.querySelector(".btn-start");
@@ -26,6 +26,10 @@ btnStart.addEventListener("click", newGame);
 btnNew.addEventListener("click", newGame);
 btnRoll.addEventListener("click", rollDice);
 btnHold.addEventListener("click", bankScore);
+
+// DOM Variables
+var diceDOM1 = document.getElementById("dice-1");
+var diceDOM2 = document.getElementById("dice-2");
 
 init();
 
@@ -70,15 +74,10 @@ function init() {
 }
 
 function rollDice() {
-	// Toggle Roll Dice Button OFF
-	btnRoll.setAttribute("disabled", true);
-	btnRoll.setAttribute("hidden", true);
+	// View Dice
+	diceDOM1.style.display = "block";
+	diceDOM2.style.display = "block";
 
-	// DOM Variables
-	var diceDOM = document.querySelector(".dice");
-
-	// Enable Dice
-	diceDOM.style.display = "block";
 	/* 
 	// Update and animate Dice Roll
 	function animateDice() {
@@ -100,21 +99,33 @@ function rollDice() {
 			finalRoll();
 		}, 750 * (diceRolls + 1));
 	}
- */
+ 	*/
 	// Display Final Result
 	function finalRoll() {
-		diceValue = Math.ceil(Math.random() * 6);
-		diceDOM.src = "dice-" + diceValue + ".png";
+		// Set Final Roll Values for Dice
+		diceValue1 = Math.ceil(Math.random() * 6);
+		diceValue2 = Math.ceil(Math.random() * 6);
+		diceDOM1.src = "dice-" + diceValue1 + ".png";
+		diceDOM2.src = "dice-" + diceValue2 + ".png";
 
-		// Update Round Score IF rolled # was NOT a 1
-		if (diceValue !== 1) {
-			// Add dice value to round score
-			roundScore += diceValue;
-			console.log("Player " + (activePlayer + 1) + " rolled a " + diceValue + ". Round total so far - " + roundScore + ".");
+		if (lastRoll === 6 & diceValue1 === 6) { // Bankrupt player if they roll two 6s
+			// Zero out active player scores
+			roundScore = 0;
+			scores[activePlayer] = 0;
+
+			console.log("BANKRUPT!!!. Player " + (activePlayer + 1) + " rolled two 6s.");
+
+			document.getElementById("score-" + activePlayer).textContent = 0;
+			document.getElementById("current-" + activePlayer).textContent = 0;
+			switchPlayer();
+		} else if (diceValue1 !== 1) {
+			roundScore += diceValue1;
+			console.log("Player " + (activePlayer + 1) + " rolled a " + diceValue1 + ". Round total so far - " + roundScore + ".");
 			document.getElementById("current-" + activePlayer).textContent = roundScore;
+			lastRoll = diceValue1;
 		} else {
 			document.getElementById("current-" + activePlayer).textContent = 0;
-			console.log("BAD LUCK!!!. Player " + (activePlayer + 1) + " rolled a 1 and lost out on " + roundScore + " points.");
+			console.log("BAD LUCK!!! Player " + (activePlayer + 1) + " rolled a 1 and lost out on " + roundScore + " points.");
 			roundScore = 0;
 			switchPlayer();
 		}
@@ -148,18 +159,27 @@ function newGame() {
 }
 
 function bankScore() {
+	var scoreInput = document.querySelector(".winning-score").value;
+	var defaultWinningScore;
+	// Undefined, 0, null or "" are COERCED to false
+	// Anything else is COERCED to true
+	if (scoreInput) {
+		defaultWinningScore = scoreInput;
+	} else {
+		defaultWinningScore = 100;
+		document.querySelector(".winning-score").value = 100;
+	}
+
 	// Bank CURRENT scort to Global Scores
-	scores[activePlayer] += roundScore;
 	console.log("Player " + (activePlayer + 1) + " banked " + roundScore + " points.");
+	scores[activePlayer] += roundScore;
 
 	// Update UI
 	document.getElementById("current-" + activePlayer).textContent = 0;
 	document.getElementById("score-" + activePlayer).textContent = scores[activePlayer];
 
-	roundScore = 0;
-
 	// Check if player won
-	if (scores[activePlayer] >= 30) {
+	if (scores[activePlayer] >= defaultWinningScore) {
 		gameWin();
 	} else {
 		switchPlayer();
@@ -167,6 +187,9 @@ function bankScore() {
 }
 
 function switchPlayer() {
+	roundScore = 0;
+	lastRoll = 0;
+
 	activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
 
 	console.log("\n");
