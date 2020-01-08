fde66c1a91f2563b54e48ebfa11574d7c96084db
abarela13 <abarela13@gmail.com>
2020-01-07 16:29:24 -0800

    Functions returning functions

--- 5-advanced-JS/final/script.js
+++ 5-advanced-JS/final/script.js
@@ -109,7 +109,7 @@ change(age, obj);
  */
 /////////////////////////////
 // Passing functions as arguments
-
+/* 
 var years = [1990, 1965, 1937, 2005, 1998];
 
 function arrayCalculation(arr, fn) {
@@ -151,38 +151,38 @@ console.log(fullAges);
 console.log("Max Heart Rates by Ages");
 console.log(years);
 console.log(rates);
-
+ */
 /////////////////////////////
 // Lecture: Functions returning functions
-/*
+
 function interviewQuestion(job) {
-    if (job === 'designer') {
-        return function(name) {
-            console.log(name + ', can you please explain what UX design is?');
-        }
-    } else if (job === 'teacher') {
-        return function(name) {
-            console.log('What subject do you teach, ' + name + '?');
-        }
-    } else {
-        return function(name) {
-            console.log('Hello ' + name + ', what do you do?');
-        }
-    }
+	if (job === "designer") {
+		// Anonymous functions (no name)
+		return function(name) {
+			console.log(name + ", can you please explain what UX design is?");
+		};
+	} else if (job === "teacher") {
+		return function(name) {
+			console.log("What subject do you teach, " + name + "?");
+		};
+	} else {
+		return function(name) {
+			console.log("Hello " + name + ", what do you do?");
+		};
+	}
 }
 
-var teacherQuestion = interviewQuestion('teacher');
-var designerQuestion = interviewQuestion('designer');
+var teacherQuestion = interviewQuestion("teacher");
+var designerQuestion = interviewQuestion("designer");
 
+teacherQuestion("John");
+designerQuestion("John");
+designerQuestion("Jane");
+designerQuestion("Mark");
+designerQuestion("Mike");
 
-teacherQuestion('John');
-designerQuestion('John');
-designerQuestion('jane');
-designerQuestion('Mark');
-designerQuestion('Mike');
-
-interviewQuestion('teacher')('Mark');
-*/
+// Returns function then calls that function with second argument
+interviewQuestion("teacher")("Mark");
 
 /////////////////////////////
 // Lecture: IIFE
