3d7b160c9f9de9ebf3bf8df2a86202e0968095d0
abarela13 <abarela13@gmail.com>
2020-01-09 12:45:55 -0800

    Closures

--- 5-advanced-JS/final/script.js
+++ 5-advanced-JS/final/script.js
@@ -219,15 +219,20 @@ function () {
 	console.log(score >= 5 - goodLuck);
 })(5);
  */
-/////////////////////////////
-// Lecture: Closures
-/*
+
+/* 
+ Lecture: Closures
+ An innner function always has access to the variables and parameters of the its outer "parent" function,
+ even after the outer function has returned.
+ */
+
 function retirement(retirementAge) {
-    var a = ' years left until retirement.';
-    return function(yearOfBirth) {
-        var age = 2018 - yearOfBirth;
-        console.log((retirementAge - age) + a);
-    }
+	var a = " years left until retirement.";
+
+	return function(yearOfBirth) {
+		var age = new Date().getFullYear() - yearOfBirth;
+		console.log(retirementAge - age + a);
+	};
 }
 
 var retirementUS = retirement(66);
@@ -240,21 +245,19 @@ retirementIceland(1990);
 
 //retirement(66)(1990);
 
-
 function interviewQuestion(job) {
-    return function(name) {
-        if (job === 'designer') {
-            console.log(name + ', can you please explain what UX design is?');
-        } else if (job === 'teacher') {
-            console.log('What subject do you teach, ' + name + '?');
-        } else {
-            console.log('Hello ' + name + ', what do you do?');
-        }
-    }
+	return function(name) {
+		if (job === "designer") {
+			console.log(name + ", can you please explain what UX design is?");
+		} else if (job === "teacher") {
+			console.log("What subject do you teach, " + name + "?");
+		} else {
+			console.log("Hello " + name + ", what do you do?");
+		}
+	};
 }
 
-interviewQuestion('teacher')('John');
-*/
+interviewQuestion("teacher")("John");
 
 /////////////////////////////
 // Lecture: Bind, call and apply
