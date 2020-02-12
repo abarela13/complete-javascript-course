768017b9fbdbbcc05297dbb42b5eeef880f0ea58
abarela13 <abarela13@gmail.com>
2020-02-12 15:53:17 -0800

    Spread Operator

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -255,20 +255,18 @@ console.log(retirement);
  */
 //////////////////////////////
 // Lecture: Arrays
-
-const boxes = document.querySelectorAll(".box");
 /* 
+const boxes = document.querySelectorAll(".box");
 // ES5
 var boxesArr5 = Array.prototype.slice.call(boxes);
 boxesArr5.forEach(function (cur) {
 	cur.style.backgroundColor = "dodgerblue";
 });
- */
 
 // ES6
 const boxesArr6 = Array.from(boxes);
 boxesArr6.forEach(cur => (cur.style.backgroundColor = "dodgerblue"));
-/*
+
 // ES5
 for (var i = 0; i < boxesArr5.length; i++) {
 	if (boxesArr5[i].className === "box blue") {
@@ -277,7 +275,6 @@ for (var i = 0; i < boxesArr5.length; i++) {
 
 	boxesArr5[i].textContent = "I changed to blue!";
 }
- */
 
 // ES6
 for (const cur of boxesArr6) {
@@ -301,3 +298,36 @@ console.log(ages[legalAge.indexOf(true)]);
 // ES6
 console.log(ages.findIndex(cur => cur >= 18));
 console.log(ages.find(cur => cur >= 18));
+ */
+
+//////////////////////////////
+// Lecture: Arrays
+
+function addFourAges(a, b, c, d) {
+	return a + b + c + d;
+}
+
+var sum1 = addFourAges(18, 30, 12, 21);
+console.log(`sum1 - ${sum1}`);
+
+// ES5
+var ages = [18, 30, 12, 21];
+var sum2 = addFourAges.apply(null, ages);
+console.log(`sum2 - ${sum2}`);
+
+// ES6
+const sum3 = addFourAges(...ages); // expands ages array into its components
+console.log(`sum3 - ${sum3}`);
+
+const familySmith = ["John", "Jane", "Mark"];
+const familyMiller = ["Mary", "Bob", "Ann"];
+const marriageSmithMiller = [...familyMiller, ...familySmith];
+console.log(familySmith);
+console.log(familyMiller);
+console.log(marriageSmithMiller);
+
+const h = document.querySelector("h1");
+const boxes = document.querySelectorAll(".box");
+const allElements = [h, ...boxes];
+
+Array.from(allElements).forEach(cur => (cur.style.color = "white"));
