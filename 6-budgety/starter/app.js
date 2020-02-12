6250c2b0740c368ff94f415214698c84fe7457f3
abarela13 <abarela13@gmail.com>
2020-02-12 11:46:17 -0800

    Destructuring

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -10,6 +10,19 @@ var budgetController = (function() {
 		this.id = id;
 		this.description = description;
 		this.value = value;
+		this.percentage = -1;
+	};
+
+	Expense.prototype.calcPercentage = function(totalIncome) {
+		if (totalIncome > 0) {
+			this.percentage = parseFloat((this.value / totalIncome) * 100).toFixed(1);
+		} else {
+			this.percentage = -1;
+		}
+	};
+
+	Expense.prototype.getPercentage = function() {
+		return this.percentage;
 	};
 
 	var calculateTotal = function(type) {
@@ -64,6 +77,23 @@ var budgetController = (function() {
 			return newItem;
 		},
 
+		deleteItem: function(type, id) {
+			var uniqueIDs, elementIndex;
+
+			// Loop over allItem elements
+			uniqueIDs = data.allItems[type].map(function(current) {
+				return current.id;
+			});
+
+			// Find array index of the target id
+			elementIndex = uniqueIDs.indexOf(id);
+
+			if (elementIndex !== -1) {
+				// Delete element from data structure
+				data.allItems[type].splice(elementIndex, 1);
+			}
+		},
+
 		calculateBudget: function() {
 			// Calculate total income and expenses
 			calculateTotal("inc");
@@ -73,7 +103,24 @@ var budgetController = (function() {
 			data.budget = data.totals.inc - data.totals.exp;
 
 			// Calculate the percentage of income that we expect
-			data.percentage = ((data.totals.exp / data.totals.inc) * 100).toFixed(2);
+			if (data.totals.inc > 0) {
+				data.percentage = parseFloat(((data.totals.exp / data.totals.inc) * 100).toFixed(1));
+			} else {
+				data.percentage = -1;
+			}
+		},
+
+		calculatePercentages: function() {
+			data.allItems.exp.forEach(function(cur) {
+				cur.calcPercentage(data.totals.inc);
+			});
+		},
+
+		getPercentages: function() {
+			var allPercentages = data.allItems.exp.map(function(cur) {
+				return cur.getPercentage();
+			});
+			return allPercentages;
 		},
 
 		getBudget: function() {
@@ -86,7 +133,7 @@ var budgetController = (function() {
 		},
 
 		testing: function() {
-			console.log(data);
+			console.log(data.allItems);
 		}
 	};
 })();
@@ -99,7 +146,37 @@ var UIController = (function() {
 		inputValue: ".add__value",
 		inputButton: ".add__btn",
 		incomeContainer: ".income__list",
-		expensesContainer: ".expenses__list"
+		expensesContainer: ".expenses__list",
+		budgetLabel: ".budget__value",
+		incomeLabel: ".budget__income--value",
+		expenseLabel: ".budget__expenses--value",
+		budgetPercentageLabel: ".budget__expenses--percentage",
+		container: ".container",
+		expensesPercentageLabel: ".item__percentage",
+		dateLabel: ".budget__title--month"
+	};
+
+	var formatNumber = function(num, type) {
+		var numSplit, int, dec, type;
+
+		/*
+		+ or - before numbers
+		exactly 2 decimal points
+		comma seperating the thousands 
+		*/
+
+		num = Math.abs(num);
+		num = num.toFixed(2);
+
+		numSplit = num.split(".");
+		int = numSplit[0];
+		if (int.length > 3) {
+			int = int.substr(0, int.length - 3) + "," + int.substr(int.length - 3, 3);
+		}
+
+		dec = numSplit[1];
+
+		return (type === "exp" ? "-" : "+") + " " + int + "." + dec;
 	};
 
 	return {
@@ -118,20 +195,25 @@ var UIController = (function() {
 			if (type === "inc") {
 				element = DOMstrings.incomeContainer;
 
-				html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">+ %value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+				html = '<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">+ %value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
 			} else if (type === "exp") {
 				element = DOMstrings.expensesContainer;
 
-				html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">- %value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+				html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">- %value%</div><div class="item__percentage"></div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
 			}
 
 			// Replace placehold text with actual data
 			newHtml = html.replace("%id%", obj.id);
 			newHtml = newHtml.replace("%description%", obj.description);
-			newHtml = newHtml.replace("%value%", obj.value);
+			newHtml = newHtml.replace("%value%", formatNumber(obj.value, type));
 
 			// Insert the HTML into the DOM
 			document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
+		},
+
+		deleteListItem: function(selectorID) {
+			var childEl = document.getElementById(selectorID);
+			childEl.parentNode.removeChild(childEl);
 		},
 
 		clearFields: function() {
@@ -146,6 +228,58 @@ var UIController = (function() {
 			});
 
 			fieldsArr[0].focus();
+		},
+
+		displayBudget: function(obj) {
+			var type;
+			obj.budget > 0 ? (type = "inc") : (type = "exp");
+
+			if (obj.budget !== 0) {
+				document.querySelector(DOMstrings.budgetLabel).textContent = formatNumber(obj.budget, type);
+			} else {
+				document.querySelector(DOMstrings.budgetLabel).textContent = "-";
+			}
+
+			document.querySelector(DOMstrings.incomeLabel).textContent = formatNumber(obj.totalInc, "inc");
+			document.querySelector(DOMstrings.expenseLabel).textContent = formatNumber(obj.totalExp, "exp");
+
+			if (obj.percentage > 0) {
+				document.querySelector(DOMstrings.budgetPercentageLabel).textContent = obj.percentage + "%";
+			} else {
+				document.querySelector(DOMstrings.budgetPercentageLabel).textContent = "-";
+			}
+		},
+
+		displayPercentages: function(percentages) {
+			// Returns Node List
+			var fields = document.querySelectorAll(DOMstrings.expensesPercentageLabel);
+
+			// Node list forEach Method
+			var nodeListForEach = function(list, callback) {
+				for (var i = 0; i < list.length; i++) {
+					callback(list[i], i);
+				}
+			};
+
+			nodeListForEach(fields, function(current, index) {
+				if (percentages[index] > 0) {
+					current.textContent = percentages[index] + "%";
+				} else {
+					current.textContent = "-";
+				}
+			});
+		},
+
+		displayDate: function() {
+			var now, month, months, year;
+
+			now = new Date();
+
+			month = now.getMonth();
+			months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+			year = now.getFullYear();
+			document.querySelector(DOMstrings.dateLabel).textContent = months[month] + " " + year;
 		},
 
 		getDomStrings: function() {
@@ -168,17 +302,9 @@ var controller = (function(budgetCtrl, UICtrl) {
 			// Display ANY keyboard event
 			// console.log(event);
 		});
-	};
 
-	var updateBudget = function() {
-		// Calculate Budget
-		budgetCtrl.calculateBudget();
-
-		// Return the Budget
-		var budget = budgetCtrl.getBudget();
-
-		// Display Budget on UI
-		console.log(budget);
+		// Event listener for div class "container"
+		document.querySelector(DOM.container).addEventListener("click", ctrlDeleteItem);
 	};
 
 	var ctrlAddItem = function() {
@@ -199,12 +325,70 @@ var controller = (function(budgetCtrl, UICtrl) {
 
 			// Calculate and update budget
 			updateBudget();
+
+			// Update percentages
+			updatePercentages();
 		}
+	};
+
+	var ctrlDeleteItem = function(event) {
+		var itemID, splitID, type, ID;
+
+		// Travers DOM to locate desired ID
+		itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+
+		if (itemID) {
+			// Example | inc-1
+			splitID = itemID.split("-");
+			type = splitID[0];
+			ID = parseInt(splitID[1]);
+
+			// Delete the item from the data structure
+			budgetCtrl.deleteItem(type, ID);
+
+			// Delete item from UI
+			UICtrl.deleteListItem(itemID);
+
+			// Update and show the new budget
+			updateBudget();
+
+			// Update percentages
+			updatePercentages();
+		}
+	};
+
+	var updateBudget = function() {
+		// Calculate Budget
+		budgetCtrl.calculateBudget();
+
+		// Return the Budget
+		var budget = budgetCtrl.getBudget();
+
+		// Display Budget on UI
+		UICtrl.displayBudget(budget);
+	};
+
+	var updatePercentages = function(type) {
+		// Calculate percentages
+		budgetCtrl.calculatePercentages();
+
+		// Read percentages from the budget controller
+		var percentages = budgetCtrl.getPercentages();
+
+		// Update UI with new percentages
+		UICtrl.displayPercentages(percentages);
 	};
 
 	return {
 		init: function() {
 			setupEventListenter();
+			UICtrl.displayDate();
+			UICtrl.displayBudget({
+				budget: 0,
+				totalInc: 0,
+				totalExp: 0,
+				percentage: -1
+			});
 			console.log("Event listeners initialized.");
 		}
 	};
---
+++ 7-ES6/starter/script.js
@@ -0,0 +1,257 @@
+/*
+ ES5
+ Variables are function scoped
+*/
+/* 
+var name5 = "Jane Smith";
+var age5 = 23;
+name = "Jane Miller"; //Mutation
+ */
+// console.log(name5); // Jane Miller
+
+/*
+ ES6
+ Variables are block scoped
+ Code wrapped between curly braces
+*/
+/* 
+const name6 = "John Smith";
+let age6 = 28; // Mutatable
+ */
+// name6 = "John Miller";
+// console.log(name6); // Error - Assignment to constant variable
+
+/*
+ ES5
+ var works within the function
+*/
+/* 
+function driverLicesne5(passedTest) {
+    if (passedTest) {
+        // console.log(firstName); // undefined, var variables are hoisted and will show undefined
+        var firstName = "John";
+        var yearOfBirth = 1990;
+    }
+    console.log(firstName + ", born in " + yearOfBirth + ", is now officially allowed to drive.");
+}
+driverLicesne5(true);
+ */
+/*
+ ES6
+ let and const only work within the curly braces
+*/
+/* 
+function driverLicesne6(passedTest) {
+    // console.log(firstName); // firstName is not defined ERROR, TEMPORAL DEAD ZONE
+    let firstName;
+    const yearOfBirth = 1986;
+
+    if (passedTest) {
+        firstName = "Jane";
+    }
+
+    console.log(firstName + ", born in " + yearOfBirth + ", is now officially allowed to drive.");
+}
+driverLicesne6(true);
+ */
+/*
+ ES5
+ let and const only work within the curly braces
+ */
+/*
+var i = 23;
+
+for (var i = 0; i < 5; i++) {
+    console.log(i); // 0 1 2 3 4
+   }
+   
+   console(i); // 5
+*/
+
+/*
+ ES6
+ let and const only work within the curly braces
+ */
+/*
+ let i = 23;
+ 
+ for (let i = 0; i < 5; i++) {
+     console.log(i);  // 0 1 2 3 4
+    }
+    
+    console(i);  // 23
+ */
+
+//////////////////////////////
+// Lecture: Block vs IIFEs
+/* 
+// ES6 Block
+{
+    const a = 1;
+    let b = 2;
+    var c = 3
+}
+console.log(a + b); // ERROR, not accessible outside of block
+console.log(c); // 3, var c is function scoped not block scoped
+
+
+// ES5 IIFE
+(function () {
+    var d = 3;
+})
+console.log(d); // ERROR, d is not defined
+ */
+
+//////////////////////////////
+// Lecture: Strings
+/* 
+let fName = "John";
+let lName = "Smith";
+const yearOfBirth = 1990;
+
+function calcAge(year) {
+    return new Date().getFullYear() - year;
+}
+
+// ES5 - Template Literals
+console.log("// ES5");
+console.log("This is " + fName + " " + lName + ". He was born in " + yearOfBirth + ". Today he is " + calcAge(yearOfBirth) + " years old.");
+
+// ES6
+console.log("\n");
+console.log("// ES6");
+console.log(`This is ${fName} ${lName}. He was born in ${yearOfBirth}. Today he is ${calcAge(yearOfBirth)} years old.`);
+const n = `${fName} ${lName}`;
+console.log(n.startsWith("J"));
+console.log(n.startsWith("j"));
+console.log(n.endsWith("th"));
+console.log(n.includes(" "));
+console.log(`${fName} `.repeat(5)); // John John John John John
+ */
+
+//////////////////////////////
+// Lecture: Arrow Functions
+/* 
+const years = [1990, 1985, 1962, 1937];
+
+// ES5
+var ages5 = years.map(function(el) {
+	return new Date().getFullYear() - el;
+});
+console.log(ages5);
+
+// ES6
+const ages6 = years.map(el => new Date().getFullYear() - el); // For callback functions with only one argument
+console.log(ages6);
+
+let ages7 = years.map((el, index) => {
+	const thisYear = new Date().getFullYear();
+	const age = thisYear - el;
+	return `Age element ${index + 1}: ${age} years old.`;
+});
+
+console.log(ages7);
+ */
+
+// Lecture: Arrow Functions - THIS
+/* 
+// ES5
+var box5 = {
+	color: "green",
+	position: 1,
+	clickMe: function () {
+		var self = this;
+
+		document.querySelector(".green").addEventListener("click", function () {
+			var str = "This is box number " + self.position + " and it is " + self.color;
+			console.log(str);
+		});
+	}
+};
+box5.clickMe();
+
+// ES6
+const box6 = {
+	color: "blue",
+	position: 1,
+	clickMe: function () {
+		document.querySelector(".blue").addEventListener("click", () => {
+			var str = "This is box number " + this.position + " and it is " + this.color;
+			console.log(str);
+		});
+	}
+};
+box6.clickMe();
+
+function Person(name) {
+	this.name = name;
+}
+
+var friends = ["Bob", "Jane", "Mark", "Steph"];
+
+// ES5
+Person.prototype.myFriends5 = function (friends) {
+	var arr = friends.map(
+		function (el) {
+			var self = this;
+			return this.name + " is friends with " + el;
+		}.bind(this) // binds the "this" keyword to whatever value is passed with bind, this case the for Person (this)
+	);
+
+	console.log(arr);
+};
+
+new Person("John").myFriends5(friends);
+
+// ES6
+Person.prototype.myFriends6 = function (friends) {
+	let arr = friends.map(el => `${this.name} is friends with ${el}.`);
+
+	console.log(arr);
+};
+
+new Person("Steph").myFriends6(friends);
+ */
+//////////////////////////////
+// Lecture: Destructuring
+/* 
+// ES5
+var john = ['John', 26];
+var name5 = john[0];
+var age5 = john[1];
+
+// ES6
+const [name6, age6] = ['John', 26];
+console.log(name6);
+console.log(age6);
+
+const obj = {
+	firstName: 'John',
+	lastName: 'Smith'
+};
+
+const {
+	firstName,
+	lastName
+} = obj;
+console.log(firstName);
+console.log(lastName);
+
+const {
+	firstName: a,
+	lastName: b
+} = obj;
+console.log(a);
+console.log(b);
+
+function calcAgeRetirement(year) {
+	const age = new Date().getFullYear() - year;
+	return [age, 65 - age];
+}
+
+const [age2, retirement] = calcAgeRetirement(1990);
+console.log(age2);
+console.log(retirement);
+ */
+//////////////////////////////
+// Lecture: Destructuring
